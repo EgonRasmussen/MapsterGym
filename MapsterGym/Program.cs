4ea9fd24194c29b2a8274f5c8ba5ac8f5453d003
EgonRasmussen <ecr@eucsyd.dk>
2024-02-13 10:07:12 +0100

    ok

--- MapsterGym/Program.cs
+++ MapsterGym/Program.cs
@@ -4,42 +4,29 @@ internal class Program
 {
     private static void Main()
     {
-        User source = new User
+        UserDto destination = new UserDto
         {
-            Address = new Address
-            {
-                ZipCode = "12345"
-            },
-
-            FirstName = "John",
-            LastName = "Doe"
+            EmailAddress = "johndoe@gmail.com"
         };
 
-        // In object flattening, we can map the nested properties to the top-level properties using a simple naming convention (Toplevelname + Sublevelname = "Address"+"ZipCode").
-        // 1. For instance, the Address.ZipCode nested property from the User source type can be mapped to AddressZipCode property in the UserDto destination type.
-        // 2. The FullName property in the UserDto destination type can be mapped to the GetFullName method in the User source type.
+        // Mapping from dto to source. The only new thing here is the TwoWays method.
 
-        UserDto destination = source.Adapt<UserDto>();  
+        TypeAdapterConfig<User, UserDto>
+            .NewConfig()
+            .TwoWays()
+            .Map(dest => dest.EmailAddress, src => src.Email);
+
+        User source = destination.Adapt<User>();
     }
 }
 
 public class User
 {
-    public Address Address { get; set; } = null!;               // 1.
-
-    public string FirstName { get; set; } = null!;              // 2.
-    public string LastName { get; set; } = null!;               // 2.
-    public string GetFullName() => $"{FirstName} {LastName}";   // 2.
+    public string Email { get; set; } = null!;
 }
 
-public class Address
-{
-    public string ZipCode { get; set; } = null!;                // 1.
-}
 
 public class UserDto
 {
-    public string AddressZipCode { get; set; } = null!;         // 1.
-
-    public string FullName { get; set; } = null!;               // 2.
+    public string EmailAddress { get; set; } = null!;
 }
