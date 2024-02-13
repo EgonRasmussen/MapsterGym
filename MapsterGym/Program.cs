d87948485b57a1666a7331de7055d45966594edb
EgonRasmussen <ecr@eucsyd.dk>
2024-02-13 09:39:42 +0100

    ok

--- MapsterGym/Program.cs
+++ MapsterGym/Program.cs
@@ -6,59 +6,29 @@ internal class Program
     {
         User source = new User
         {
-            Id = 1,
-            Name = "User 1",
-            Email = "test@example.com",
-            IsActive = true,
-            CreatedAt = DateTime.Now,
-            Address = new Address
-            {
-                AddressLine1 = "Address Line 1",
-                AddressLine2 = "Address Line 2",
-                City = "City",
-                State = "State",
-                Country = "Country",
-                ZipCode = "ZipCode"
-            }
+            FirstName = "John",
+            LastName = "Doe"
         };
 
-        // Create new instance of UserDto and map the properties from source to destination
-        // Simple Type Mapping: Konverterer typen for CreatedAt fra DateTime til string
-        // Nested Type Mapping: Konverterer Address til Address
-        UserDto destination = source.Adapt<UserDto>();  
+
 
         // Map to existing instance
         UserDto destination1 = new();
+
+        TypeAdapterConfig<User, UserDto>
+            .NewConfig()
+            .Map(dest => dest.FullName, src => $"{src.FirstName} {src.LastName}");
         source.Adapt(destination1);
     }
 }
 
 public class User
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public bool IsActive { get; set; }
-    public string Email { get; set; } = null!;
-    public DateTime CreatedAt { get; set; }
-    public Address Address { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
 }
 
 public class UserDto
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public bool IsActive { get; set; }
-    public string Email { get; set; } = null!;
-    public string CreatedAt { get; set; } = null!;
-    public Address Address { get; set; } = null!;
-}
-
-public class Address
-{
-    public string AddressLine1 { get; set; } = null!;
-    public string AddressLine2 { get; set; } = null!;
-    public string City { get; set; } = null!;
-    public string State { get; set; } = null!;
-    public string Country { get; set; } = null!;
-    public string ZipCode { get; set; } = null!;
+    public string FullName { get; set; } = null!;
 }
